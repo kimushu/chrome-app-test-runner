874877479b57bd08b7909926a251722c0b796be7
kimu_shu <alfvegardrisc@gmail.com>
2016-03-26 15:49:40 +0900

    Copy scripts into temporary directory

--- testFilePrep.js
+++ testFilePrep.js
@@ -2,7 +2,8 @@ module.exports = function(filePath, scriptList) {
   var fs = require("fs"),
   execSync = require("child_process").execSync,
   path = require("path"),
-  shelljs = require("shelljs");
+  shelljs = require("shelljs"),
+  loadScriptsPath = path.join(__dirname, "load-scripts");
 
   cleanLaunchEnvironment();
   browserifyTestFile(path.join(process.cwd(), filePath));
@@ -15,6 +16,8 @@ module.exports = function(filePath, scriptList) {
     shelljs.rm("-f", path.join(__dirname, "test-runner.html"));
     shelljs.rm("-rf", tempChromeDataPath);
     shelljs.mkdir(tempChromeDataPath);
+    shelljs.rm("-rf", loadScriptsPath);
+    shelljs.mkdir(loadScriptsPath);
     shelljs.cp(path.join(__dirname, "First Run"), tempChromeDataPath);
   }
 
@@ -29,14 +32,17 @@ module.exports = function(filePath, scriptList) {
 
   function makeRunner(scriptList) {
     var fileText = fs.readFileSync(path.join(__dirname, "test-runner.html.tmpl"), "utf-8");
-    var scr = "";
+    var newText = "";
     if (scriptList) {
       scripts = scriptList.split(",");
       for (index in scripts) {
-        scr += "<script src=\"" + scripts[index] + "\" charset=\"utf-8\"></script>";
+        script = scripts[index];
+        name = path.basename(script);
+        shelljs.cp(script, path.join(loadScriptsPath, name));
+        newText += "<script src=\"load-scripts/" + name + "\" charset=\"utf-8\"></script>";
       }
     }
-    fs.writeFileSync(path.join(__dirname, "test-runner.html"), fileText.replace("<!--LOAD-SCRIPTS-->", scr));
+    fs.writeFileSync(path.join(__dirname, "test-runner.html"), fileText.replace("<!--LOAD-SCRIPTS-->", newText));
   }
 
 };
